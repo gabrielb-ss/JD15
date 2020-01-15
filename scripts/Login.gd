de7bc4963fa34109be4f8698824eb9271d5cf8ba
Gabriel Barbosa <1stmopt@gmail.com>
2020-01-15 11:36:42 -0200

    Tela de login feita

---
+++ scripts/Login.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+func _ready():
+	if PlayerInfo.load_player() == 0:
+		get_tree().change_scene("res://scenes/MainMenu.tscn")
+		print(PlayerInfo.username)
+
+func _on_LineEdit_text_entered(new_text):
+	PlayerInfo.username = $LineEdit.text
+	PlayerInfo.save_player()
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
